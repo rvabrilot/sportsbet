616e7caf6669306075400b6b68bd265117fd3a60
rvabrilot <rvabrilot@gmail.com>
2022-10-11 16:49:15 -0300

    bet post,delete,get

--- backend/sportsbet_server/controllers/bet_controller.py
+++ backend/sportsbet_server/controllers/bet_controller.py
@@ -1,19 +1,42 @@
-import connexion
-import six
+import connexion, uuid
+from flask import make_response, abort
+from sportsbet_server.config import db
+from sportsbet_server.models import Bet, BetSchema
 
-from sportsbet_server.models import Bet
 
+def add_bet(bet=None):
 
-def add_bet(user_id, event_ids, amount, api_key=None):
+    if connexion.request.is_json:
+        bet = connexion.request.get_json()
     
-    return 'do some magic!'
+    existing_bet = (Bet.query.filter(Bet.event_id == bet['event_id'])
+    .filter(Bet.user_id == bet["user_id"])
+    .one_or_none()
+    )
+
+    if existing_bet is None:
+        schema = BetSchema()
+        new_bet = schema.load(bet, session=db.session)
+        db.session.add(new_bet)
+        db.session.commit()
+    
+        return make_response(schema.dump(new_bet), 201)
+    else:
+        abort(409, f"Bet for user_id: {bet['user_id']} and event_id:{bet['event_id']} already exists")
 
 
-def delete_bet(bet_id, api_key=None):
+def delete_bet(id_):
+    bet = Bet.query.filter(Bet.id == uuid.UUID(id_)).one_or_none()
+    if bet is not None:
+        db.session.delete(bet)
+        db.session.commit()
+    return make_response(f"Bet {id_} deleted", 204)
 
-    return 'do some magic!'
 
-
-def get_bet_by_id(bet_id, api_key=None):
-
-    return 'do some magic!'
+def get_bet_by_id(id_):
+    bet = Bet.query.filter(Bet.id == uuid.UUID(id_)).one_or_none()
+    if bet is not None:
+        data = BetSchema().dump(bet)
+        return make_response(data, 200)
+    else:
+        abort(404, f"Bet not found for id: {id_}")
