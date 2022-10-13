d1aeed9aff95c634098a3fabd3e283d4f689ddd8
rvabrilot <rvabrilot@gmail.com>
2022-10-13 15:52:39 -0300

    endurecimiento de creacion y actualizacion de eventos y apuestas

--- backend/sportsbet_server/controllers/bet_controller.py
+++ backend/sportsbet_server/controllers/bet_controller.py
@@ -1,8 +1,8 @@
 import connexion, uuid
 from flask import make_response, abort
 from sportsbet_server.config import db
-from sportsbet_server.models import Bet, BetSchema
-
+from sportsbet_server.models import Bet, BetSchema, Event, EventSchema
+from datetime import datetime
 
 def add_bet(bet=None):
 
@@ -15,6 +15,21 @@ def add_bet(bet=None):
     .filter(Bet.user_id == uuid.UUID(bet["user_id"]))
     .one_or_none()
     )
+    existing_event = (Event.query.filter(Event.id == uuid.UUID(bet['event_id']))
+    .one_or_none()
+    )
+
+    if existing_event is None:
+        return make_response("invalid event id", 400)
+    else:
+        if existing_event.event_start <= datetime.now():
+            return make_response("cannot create bet if event has started", 400)
+
+    if int(bet["goals"]) < 0 :
+        return make_response("goals cant be less than cero", 400)
+    
+    if bet["result"] not in ['l','e','v']:
+        return make_response("invalid result, has to be l, e or v", 400)
 
     if existing_bet is None:
         schema = BetSchema()
@@ -42,7 +57,27 @@ def update_bet(bet=None):
     existing_bet = (Bet.query.filter(Bet.id == uuid.UUID(bet['id']))
         .one_or_none()
     )
+    try:
+       int(bet["goals"])
+    except Exception as e:
+        return make_response(str(e), 400) 
 
+    existing_event = (Event.query.filter(Event.id == uuid.UUID(bet['event_id']))
+    .one_or_none()
+    )
+
+    if existing_event is None:
+        return make_response("invalid event id", 400)
+    else:
+        if existing_event.event_start <= datetime.now():
+            return make_response("cannot update bet if event has started", 400)
+
+    if int(bet["goals"]) < 0 :
+        return make_response("goals cant be less than cero", 400)
+    
+    if bet["result"] not in ['l','e','v']:
+        return make_response("invalid result, has to be l, e or v", 400)
+    
     if existing_bet is not None:
         schema = BetSchema()
         existing_bet.event_id = uuid.UUID(bet['event_id'])
--- backend/sportsbet_server/controllers/event_controller.py
+++ backend/sportsbet_server/controllers/event_controller.py
@@ -2,6 +2,7 @@ import connexion, uuid
 from flask import make_response, abort
 from sportsbet_server.config import db
 from sportsbet_server.models import Event, EventSchema
+from datetime import datetime
 
 def add_event():
     if connexion.request.is_json:
@@ -16,10 +17,26 @@ def add_event():
         .filter(Event.category == uuid.UUID(event['category']))
         .one_or_none()
     )
+    try:
+        event_start = datetime.fromisoformat(event['event_start'])
+        event_end = datetime.fromisoformat(event['event_end'])
+        now = datetime.now()
+    except Exception as e:
+        return make_response(str(e), 400)
+
+    if event_end <= event_start:
+        return make_response(f"event_end cannot be the same or previous than event_start", 400)
+    if event_start <= now:
+        return make_response(f"event_start cannot be previous than now", 400)
+    if event_end <= now:
+        return make_response(f"event_end cannot be previous than now", 400)
+
+    if event['visitor_player'] == event['local_player']:
+        return make_response(f"local_player and visitor_player must be different", 400)
 
     if existing_user is None:
         schema = EventSchema()
-        new_event = Event() #schema.load(event, session=db.session)
+        new_event = Event()
         new_event.id = uuid.uuid1()
         new_event.category = uuid.UUID(event['category'])
         new_event.local_player = uuid.UUID(event['local_player'])
@@ -59,6 +76,30 @@ def update_event():
         Event.query.filter(Event.id == uuid.UUID(event["id"]))
         .one_or_none()
     )
+
+    try:
+        event_start = datetime.fromisoformat(event['event_start'])
+        event_end = datetime.fromisoformat(event['event_end'])
+        now = datetime.now()
+    except Exception as e:
+        return make_response(str(e), 400)
+
+    if event_end <= event_start:
+        return make_response(f"event_end cannot be the same or previous than event_start", 400)
+    if event_start <= now:
+        return make_response(f"event_start cannot be previous than now", 400)
+    if event_end <= now:
+        return make_response(f"event_end cannot be previous than now", 400)
+
+    if event['visitor_player'] == event['local_player']:
+        return make_response(f"local_player and visitor_player must be different", 400)
+
+    if int(event["goals"]) < 0 :
+        return make_response("goals cant be less than cero", 400)
+    
+    if event["result"] not in ['l','e','v']:
+        return make_response("invalid result, has to be l, e or v", 400)
+
     if existing_event is not None:
         schema = EventSchema()
         existing_event.category = uuid.UUID(event['category'])
